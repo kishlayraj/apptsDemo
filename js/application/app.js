d7a3bca34fa1392b26590d28e753255eb61573d6
Jesus Fernandez <jesus.fndz@gmail.com>
2012-07-15 15:18:23 -0400

    Cleaning up a bit and ignoring phpStorm's idea folder
    replace tabs with spaces, reformat code for easier reading and add
    .idea folder to the .gitignore file.

--- js/application/app.js
+++ js/application/app.js
@@ -1,101 +1,103 @@
 // If you want to prevent dragging, uncomment this section
 /*
-function preventBehavior(e) 
-{ 
-  e.preventDefault(); 
-};
-document.addEventListener("touchmove", preventBehavior, false);
-*/
-	
+ function preventBehavior(e)
+ {
+ e.preventDefault();
+ };
+ document.addEventListener("touchmove", preventBehavior, false);
+ */
+
 /* If you are supporting your own protocol, the var invokeString will contain any arguments to the app launch.
-see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
-for more details -jm */
+ see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+ for more details -jm */
 /*
-function handleOpenURL(url)
-{
-	// TODO: do something with the url passed in.
-}
-*/
-	
-function onBodyLoad()
-{		
-	document.addEventListener("deviceready", onDeviceReady, false);
+ function handleOpenURL(url)
+ {
+ // TODO: do something with the url passed in.
+ }
+ */
+
+function onBodyLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 Navigation = {
-  Initialise: function () {
-    $(":jqmData(role='page')").on("pageshow", function (event) {
-      var menuStatus;
-      menuStatus = false;
-      /* Sidebar Control */
-      $(".controls a").click(function() {
-        if(menuStatus != true) {
-          $(".ui-page-active, .ui-header-fixed").animate({
-            marginLeft: "250px",
-          }, 300, function(){menuStatus = true});
-          return false;
-        } else {
-            $(".ui-page-active, .ui-header-fixed").animate({
-            marginLeft: "0px",
-          }, 300, function(){menuStatus = false});
-            return false;
-        }
-      });
-    });
-  }
+    Initialise:function () {
+        $(":jqmData(role='page')").on("pageshow", function (event) {
+            var menuStatus;
+            menuStatus = false;
+            /* Sidebar Control */
+            $(".controls a").click(function () {
+                if (menuStatus != true) {
+                    $(".ui-page-active, .ui-header-fixed").animate({
+                        marginLeft:"250px",
+                    }, 300, function () {
+                        menuStatus = true
+                    });
+                    return false;
+                } else {
+                    $(".ui-page-active, .ui-header-fixed").animate({
+                        marginLeft:"0px",
+                    }, 300, function () {
+                        menuStatus = false
+                    });
+                    return false;
+                }
+            });
+        });
+    }
 };
 
 function viewTemplate() {
-  this.getPath = function(name) {
-    return '../../templates/_' + name + '.tmpl.html';
-  },
-  this.renderExternalTemplate = function(item) {
-    var file = this.getPath( item.name );
+    this.getPath = function (name) {
+        return '../../templates/_' + name + '.tmpl.html';
+    },
+        this.renderExternalTemplate = function (item) {
+            var file = this.getPath(item.name);
 
-    $.when($.get(file))
-      .done(function(tmplData) {
-         $.templates({ tmpl: tmplData });
-         $(item.selector).html($.render.tmpl(item.data));
-      });
-  }
+            $.when($.get(file))
+                .done(function (tmplData) {
+                    $.templates({ tmpl:tmplData });
+                    $(item.selector).html($.render.tmpl(item.data));
+                });
+        }
 }
 
 
-$(document).bind("pageinit", function() {
-  var template = new viewTemplate();
+$(document).bind("pageinit", function () {
+    var template = new viewTemplate();
 
-  var menu = [
-    { name: "Home", url: "index.html" },
-    { name: "My Account", url: "my-account.html" },
-    { name: "My Appointments", url: "my-appointments.html" }
-  ];
+    var menu = [
+        { name:"Home", url:"index.html" },
+        { name:"My Account", url:"my-account.html" },
+        { name:"My Appointments", url:"my-appointments.html" }
+    ];
 
-  // TODO: Figure out how to read new menu items from each individual pages.
-  
-  template.renderExternalTemplate({ name: 'menuTemplate', selector: '#navigation', data: menu });
+    // TODO: Figure out how to read new menu items from each individual pages.
 
-  Navigation.Initialise();
+    template.renderExternalTemplate({ name:'menuTemplate', selector:'#navigation', data:menu });
+
+    Navigation.Initialise();
 });
 
 /* When this function is called, Cordova has been initialized and is ready to roll */
 /* If you are supporting your own protocol, the var invokeString will contain any arguments to the app launch.
-see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
-for more details -jm */
-function onDeviceReady()
-{
-  // do your thing!
-  var template = new viewTemplate();
+ see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+ for more details -jm */
+function onDeviceReady() {
+    // do your thing!
+    var template = new viewTemplate();
 
-  var menu = [
-    { name: "Home", url: "index.html" },
-    { name: "My Account", url: "my-account.html" },
-    { name: "My Appointments", url: "my-appointments.html" }
-  ];
+    var menu = [
+        { name:"Home", url:"index.html" },
+        { name:"My Account", url:"my-account.html" },
+        { name:"My Appointments", url:"my-appointments.html" }
+    ];
 
-  // TODO: Figure out how to read new menu items from each individual pages.
-  
-  template.renderExternalTemplate({ name: 'menuTemplate', selector: '#navigation', data: menu });
+    // TODO: Figure out how to read new menu items from each individual pages.
 
-  Navigation.Initialise();
-  navigator.notification.alert("Cordova is working");
+    template.renderExternalTemplate({ name:'menuTemplate', selector:'#navigation', data:menu });
+
+    Navigation.Initialise();
+    navigator.notification.alert("Cordova is working");
 }
