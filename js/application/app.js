065788c07559a2109444599e2a9b6c2fb1e7a130
Jesus Fernandez <jesus.fndz@gmail.com>
2012-07-17 23:53:50 -0400

    Updating dev branch with cleaned up slide out navigation and working jsrender template.

--- js/application/app.js
+++ js/application/app.js
@@ -17,6 +17,8 @@
  }
  */
 
+var menuStatus = menuStatus || false;
+
 function onBodyLoad() {
     document.addEventListener("deviceready", onDeviceReady, false);
 }
@@ -24,23 +26,27 @@ function onBodyLoad() {
 Navigation = {
     Initialise:function () {
         $(":jqmData(role='page')").on("pageshow", function (event) {
-            var menuStatus;
-            menuStatus = false;
+
+            console.log("Current Status: " + menuStatus);
+
             /* Sidebar Control */
             $(".controls a").click(function () {
+                console.log("Clicked event fired...");
                 if (menuStatus != true) {
-                    $(".ui-page-active, .ui-header-fixed").animate({
-                        marginLeft:"250px",
+                    $(".ui-page-active .main, .ui-header-fixed").animate({
+                        marginLeft:"260px"
                     }, 300, function () {
                         menuStatus = true
                     });
+                    console.log("menuStatus is false...");
                     return false;
                 } else {
-                    $(".ui-page-active, .ui-header-fixed").animate({
-                        marginLeft:"0px",
+                    $(".ui-page-active .main, .ui-header-fixed").animate({
+                        marginLeft:"0px"
                     }, 300, function () {
                         menuStatus = false
                     });
+                    console.log("menuStatus is true...");
                     return false;
                 }
             });
@@ -50,22 +56,30 @@ Navigation = {
 
 function viewTemplate() {
     this.getPath = function (name) {
-        return '../../templates/_' + name + '.tmpl.html';
+        console.log("Loading " + name);
+        return './templates/_' + name + '.tmpl.html';
     },
         this.renderExternalTemplate = function (item) {
             var file = this.getPath(item.name);
 
+            console.log(file);
             $.when($.get(file))
                 .done(function (tmplData) {
                     $.templates({ tmpl:tmplData });
-                    $(item.selector).html($.render.tmpl(item.data));
+                    console.log($.render.tmpl(item.data));
+                    $(item.selector).html(
+                        // This .render.tmpl() does not work on phonegap :( --jesusOmar
+                        $.render.tmpl(item.data)
+                    );
+                    console.log("Done");
                 });
         }
 }
 
 
 $(document).bind("pageinit", function () {
-    var template = new viewTemplate();
+    console.log("Page INIT has been called");
+    //var template = new viewTemplate(); // Function is broken... --jesusOmar
 
     var menu = [
         { name:"Home", url:"index.html" },
@@ -75,7 +89,11 @@ $(document).bind("pageinit", function () {
 
     // TODO: Figure out how to read new menu items from each individual pages.
 
-    template.renderExternalTemplate({ name:'menuTemplate', selector:'#navigation', data:menu });
+    //template.renderExternalTemplate({ name:'menuTemplate', selector:'.navigation', data:menu }); // Function is broken... --jesusOmar
+
+    $(".navigation").html(
+        $("#menuTemplate").render(menu)
+    );
 
     Navigation.Initialise();
 });
@@ -85,19 +103,5 @@ $(document).bind("pageinit", function () {
  see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
  for more details -jm */
 function onDeviceReady() {
-    // do your thing!
-    var template = new viewTemplate();
-
-    var menu = [
-        { name:"Home", url:"index.html" },
-        { name:"My Account", url:"my-account.html" },
-        { name:"My Appointments", url:"my-appointments.html" }
-    ];
-
-    // TODO: Figure out how to read new menu items from each individual pages.
-
-    template.renderExternalTemplate({ name:'menuTemplate', selector:'#navigation', data:menu });
-
-    Navigation.Initialise();
     navigator.notification.alert("Cordova is working");
 }
